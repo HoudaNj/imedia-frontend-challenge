43ab6878f4fcf846ba71404f973041f54964546b
HoudaNJ <nenjibi@gmail.com>
2022-09-14 21:46:51 +0100

    first commit

---
+++ build/precache-manifest.c9143bf6b2327c2a5cd75e9e0d261800.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1360b7c1651ee49f50fa",
+    "url": "/ReactDex/static/css/main.af8e8a4b.chunk.css"
+  },
+  {
+    "revision": "1360b7c1651ee49f50fa",
+    "url": "/ReactDex/static/js/main.1360b7c1.chunk.js"
+  },
+  {
+    "revision": "df417c7f6736df78932b",
+    "url": "/ReactDex/static/css/1.ce789555.chunk.css"
+  },
+  {
+    "revision": "df417c7f6736df78932b",
+    "url": "/ReactDex/static/js/1.df417c7f.chunk.js"
+  },
+  {
+    "revision": "bf8c3a743c6b72ed2dd3",
+    "url": "/ReactDex/static/js/runtime~main.bf8c3a74.js"
+  },
+  {
+    "revision": "2acde16eba2a74bfd6f3d59380cf385c",
+    "url": "/ReactDex/index.html"
+  }
+];
